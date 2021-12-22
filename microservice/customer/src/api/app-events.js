a7cec5093df21675133337b0510a09cbf0fb294f
Md Shayon <mdshayon0@gmail.com>
2021-12-22 11:35:58 +0600

    customers service is working perfectly

---
+++ microservice/customer/src/api/app-events.js
@@ -0,0 +1,12 @@
+const CustomerService = require('../services/customer-service');
+
+module.exports = (app) => {
+    const service = new CustomerService();
+    // curl --location --request POST 'http://localhost:8000/customer/app-events' --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtpbHlvbkBwc2cuY29tIiwiX2lkIjoiNjFjMWJlZmQxY2VmMGYzYzkzNDcyYTJjIiwiaWF0IjoxNjQwMDg3NzA4LCJleHAiOjE2NDAxNzQxMDh9.Zokz5RdJyBD5XSC-nubvGfwWTQ8tUKE4Jlp52KNb7Po' --header 'Content-Type: application/json' --data-raw '{ "payload": { "event": "New Event", "data": {} } }'
+    app.use('/app-events', async (req, res, next) => {
+        const { payload } = req.body;
+        service.SubscribeEvents(payload);
+        console.log("------------Shopping service recieved event--------");
+        return res.status(200).json(payload);
+    });
+}
